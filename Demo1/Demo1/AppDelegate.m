7de66b43a1e85227a60bf30cb986270f1fb7b1fa
devilguoguo <704648733@qq.com>
2015-09-21 21:11:17 +0800

    Demo1
    简单的表视图例子，集合了自定义表单元（代码实现与nib文件实现都有）、索引栏、搜索栏等控件内容，数据模型使用了plist属性表配置数据，项目中包
    含了objective-c的相关知识使用，包括：属性、协议、委托、代码块（block）、谓词等。

---
+++ Demo1/Demo1/AppDelegate.m
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.m
+//  Demo1
+//
+//  Created by devil on 15/9/15.
+//  Copyright (c) 2015年 devil. All rights reserved.
+//
+/**
+ 简单的表视图例子，集合了自定义表单元（代码实现与nib文件实现都有）、索引栏、搜索栏等控件内容，数据模型使用了plist属性表配置数据，项目中包含了objective-c的相关知识使用，包括：属性、协议、委托、代码块（block）、谓词等。
+ **/
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
